b60813801e4e01c0a84b7738e14106beff640ed9
Amal Joseph Varghese <amaljova@gmail.com>
2021-10-09 07:28:28 +0530

    Lag Corrected

--- assets/js/main.js
+++ assets/js/main.js
@@ -177,44 +177,44 @@ const sr = ScrollReveal({
 
 /*SCROLL HOME*/
 sr.reveal('.home_title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home_img',{delay: 320}); 
-sr.reveal('.home_social-icon',{ interval: 200}); 
-sr.reveal('.home_description',{delay: 300});
-sr.reveal('.home_subtitle',{delay: 380});
-sr.reveal('.home_scroll',{delay: 350});
+sr.reveal('.button',{delay: 100}); 
+sr.reveal('.home_img',{delay: 160}); 
+sr.reveal('.home_social-icon',{ interval: 100}); 
+sr.reveal('.home_description',{delay: 150});
+sr.reveal('.home_subtitle',{delay: 190});
+sr.reveal('.home_scroll',{delay: 175});
 
 
-sr.reveal('.section_title',{delay: 200});
-sr.reveal('.section_subtitle',{delay: 150});
+sr.reveal('.section_title',{delay: 100});
+sr.reveal('.section_subtitle',{delay: 75});
 
 /*SCROLL ABOUT*/
-sr.reveal('.about_img',{delay: 225}); 
-sr.reveal('.about_subtitle',{delay: 270}); 
-sr.reveal('.about_text',{delay: 350});
-sr.reveal('.about_description',{delay: 300});
-sr.reveal('.info_animate',{ interval: 200}); 
+sr.reveal('.about_img',{delay: 112}); 
+sr.reveal('.about_subtitle',{delay: 135}); 
+sr.reveal('.about_text',{delay: 175});
+sr.reveal('.about_description',{delay: 150});
+sr.reveal('.info_animate',{ interval: 100}); 
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills_subtitle',{}); 
 sr.reveal('.skills_text',{}); 
-sr.reveal('.skills_content',{interval: 200});
-sr.reveal('.skills_data',{interval: 350});
-sr.reveal('.skills_icon',{delay: 380});
+sr.reveal('.skills_content',{interval: 100});
+sr.reveal('.skills_data',{interval: 175});
+sr.reveal('.skills_icon',{delay: 190});
 /*SCROLL QUALIFICATION*/
-sr.reveal('.qualification_sections',{interval: 200});
-sr.reveal('.qualification_button',{interval: 270});
+sr.reveal('.qualification_sections',{interval: 100});
+sr.reveal('.qualification_button',{interval: 135});
 
 
 
 sr.reveal('.services_modality',{ interval: 250});
-sr.reveal('.services_icon',{ interval: 350});  
+sr.reveal('.services_icon',{ interval: 175});  
 /*SCROLL WORK*/
-sr.reveal('.work_img',{interval: 200}); 
+sr.reveal('.work_img',{interval: 100}); 
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact_information',{ interval: 350});
-sr.reveal('.contact_label',{ interval: 320});
-sr.reveal('.contact_icon',{ interval: 300}); 
-sr.reveal('.contact_subtitle',{delay: 380}); 
-sr.reveal('.contact_content',{interval: 200}); 
+sr.reveal('.contact_information',{ interval: 175});
+sr.reveal('.contact_label',{ interval: 160});
+sr.reveal('.contact_icon',{ interval: 150}); 
+sr.reveal('.contact_subtitle',{delay: 190}); 
+sr.reveal('.contact_content',{interval: 100}); 
